4e434dbb12e0a20bbd5c5f73cd799f659d5c5e02
pabrivjim <parijimenez@gmail.com>
2022-01-14 14:33:52 +0100

    final commit

--- src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
@@ -1,5 +1,15 @@
 package org.springframework.samples.petclinic.vacination;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +17,19 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Vaccine {
-    Integer id;
+@Entity
+@Table(name = "vaccines")
+public class Vaccine extends BaseEntity{
+    @NotNull
+    @Size(min=3, max=50)
+    @Column(unique = true)
     String name;
+    @Min(value = 0L, message = "The value must be positive")
     Double price;
-    PetType petType;
+
+    @NotNull
+    @ManyToOne
+	@JoinColumn(name = "pet_type_id")
+	private PetType petType;
+    
 }
